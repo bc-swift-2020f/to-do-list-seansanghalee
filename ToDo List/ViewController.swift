d33fd880040a2401bb2fb39c90834a67a20aed0d
Sangha Lee <tkdgklee@gmail.com>
2020-09-23 14:01:20 -0400

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Sangha Lee on 9/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
