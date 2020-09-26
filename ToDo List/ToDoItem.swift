c9dcc475cebc1dabfcd6e32603cd892b0ce9e0ef
Sangha Lee <tkdgklee@gmail.com>
2020-09-25 23:30:06 -0400

    5.6 Creating Custom Structs and Arrays of Structs

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Sangha Lee on 9/25/20.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
